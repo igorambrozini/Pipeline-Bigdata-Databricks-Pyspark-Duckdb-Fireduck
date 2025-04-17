303c785c23baa5ba85a531fe6dabe842a85021ac
Igor Ambrozini <igor@densyx.com.br>
2025-04-16 22:34:04 -0300

    Update create_dataset.csv

--- create_dataset_csv.py
+++ create_dataset_csv.py
@@ -2,14 +2,14 @@ import os
 import random
 import time
 
+
 def construir_lista_estacoes_meteorologicas():
     """
-    Obtem os nomes das estacoes meteorologicas a partir de um arquivo e remove duplicatas.
+    Obtém os nomes das estações meteorológicas a partir de um arquivo e remove duplicatas.
     """
+    with open('./data/amostra_44k.csv', 'r', encoding="utf-8") as arquivo:
+        return list(set(linha.split(';')[0] for linha in arquivo if "#" not in linha))
 
-    with open('./data/amostra_44k.csv', 'r', encoding='utf-8') as arquivo:
-        return list(set(linha.split(';')[0] for linha in arquivo if '#' not in linha))
-    
 
 def converter_bytes(num):
     """
@@ -20,7 +20,8 @@ def converter_bytes(num):
             return f"{num:.1f} {unidade}"
         num /= 1024
 
-def formatar_tempo_corrido(segundos):
+
+def formatar_tempo_decorrido(segundos):
     """
     Formata o tempo decorrido de forma simplificada.
     """
@@ -30,30 +31,31 @@ def formatar_tempo_corrido(segundos):
 
 def gerar_dados_teste(num_registros):
     """
-    Gera e escreve um arquivo com medicoes sintéticas de temperatura.
+    Gera e escreve um arquivo com medições sintéticas de temperatura.
     """
-
-    inicio_tempo = time.time() #tempo processando
+    inicio_tempo = time.time()
     nomes_estacoes = construir_lista_estacoes_meteorologicas()
     estacoes_10k_max = random.choices(nomes_estacoes, k=10_000)
     arquivo_saida = f"./data/medicoes_{num_registros}.txt"
-    tamanho_lote = 10_000 #processamento em lotes
+    tamanho_lote = 10_000  # Processamento em lotes
 
-    print (f"Criando {arquivo_saida}...")
+    print(f"Criando {arquivo_saida}...")
 
     try:
-        with open(arquivo_saida, 'w', encoding='utf-8') as arquivo:
+        with open(arquivo_saida, 'w', encoding="utf-8") as arquivo:
             for _ in range(num_registros // tamanho_lote):
                 lote = random.choices(estacoes_10k_max, k=tamanho_lote)
                 linhas = '\n'.join([f"{estacao};{random.uniform(-99.9, 99.9):.1f}" for estacao in lote])
-        
+                arquivo.write(linhas + '\n')
+
         tamanho_arquivo = os.path.getsize(arquivo_saida)
         print(f"Arquivo gerado: {arquivo_saida}")
         print(f"Tamanho final: {converter_bytes(tamanho_arquivo)}")
-        print(f"Tempo decorrido: {formatar_tempo_corrido(time.time() - inicio_tempo)}")
+        print(f"Tempo decorrido: {formatar_tempo_decorrido(time.time() - inicio_tempo)}")
     except Exception as e:
         print("Erro ao criar o arquivo:", e)
 
+
 if __name__ == "__main__":
-    num_registros = 1_000_000 # numero de registros parametrizado
+    num_registros = 1_000_000_000  # Número de registros parametrizado
     gerar_dados_teste(num_registros)
