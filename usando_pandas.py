ce034931f629504254f9dbe63136c838744f5f0b
Igor Ambrozini <igor@densyx.com.br>
2025-04-20 03:23:15 -0300

    starting code

---
+++ usando_pandas.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from multiprocessing import Pool, cpu_count
+from tqmd import tqmd # progress bar
+
+CONCURRENCY = cpu_count()
+
+total_linhas = 1_000_000_000 # total lines
+chunksize = 100_000_000 # chunk size
+filename = 'data/measurements_1000000000.txt' # file path
